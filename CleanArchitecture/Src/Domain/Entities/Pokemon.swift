3594e46cc51ecf477790f4d317f2e9b59b3207de
maurigzo <ramirezgonzalomauricio@gmail.com>
2025-01-12 00:23:31 -0300

    Create: Entities folder

---
+++ CleanArchitecture/Src/Domain/Entities/Pokemon.swift
@@ -0,0 +1,11 @@
+//
+//  Pokemon.swift
+//  CleanArchitecture
+//
+//  Created by Gonzalo Mauricio Ramirez on 11/01/2025.
+//
+
+struct Pokemon: Codable {
+    let id: String
+    let name: String
+}

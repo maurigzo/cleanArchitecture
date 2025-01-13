11573284290b87088ea2557f47cfc8cf484c42b7
maurigzo <ramirezgonzalomauricio@gmail.com>
2025-01-13 12:14:32 -0300

    Inject: view model

--- CleanArchitecture/Src/Presentation/PokemonList/PokemonListViewControllerWrapper.swift
+++ CleanArchitecture/Src/Presentation/PokemonList/PokemonListViewControllerWrapper.swift
@@ -1,16 +1,19 @@
+//
+//  PokemonListViewControllerWrapper.swift
+//  CleanArchitecture
+//
+//  Created by Gonzalo Mauricio Ramirez on 12/01/2025.
+//
+
 import SwiftUI
 import UIKit
 
 struct PokemonListViewControllerWrapper: UIViewControllerRepresentable {
-    let viewModel: PokemonListViewModel // Tu ViewModel de Combine para la lista
+    let viewModel: PokemonListViewModel
 
     func makeUIViewController(context: Context) -> PokemonListViewController {
-        let viewController = PokemonListViewController()
-        viewController.viewModel = viewModel
-        return viewController
+        PokemonListViewController(viewModel: viewModel)
     }
 
-    func updateUIViewController(_ uiViewController: PokemonListViewController, context: Context) {
-        // Aquí puedes sincronizar cambios del ViewModel o de SwiftUI hacia el UIViewController
-    }
+    func updateUIViewController(_ uiViewController: PokemonListViewController, context: Context) {}
 }
